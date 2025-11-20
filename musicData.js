35133a225df59d4c4f86a6403d7b7681785fa6c8
Kuatama <bigbabyer@stu.kust.edu.cn>
2025-11-20 10:33:56 +0800

    Create musicData.js

---
+++ musicData.js
@@ -0,0 +1,28 @@
+// 音乐数据配置文件
+const musicData = [
+  {
+    title: "想你时风起",
+    artist: "单依纯",
+    url: "https://file.notion.so/f/f/893be7b5-a484-4f7e-95dc-b29a7334966a/fb8980fd-e76c-4947-8a6d-021aec52ea90/%E5%8D%95%E4%BE%9D%E7%BA%AF_-_%E6%83%B3%E4%BD%A0%E6%97%B6%E9%A3%8E%E8%B5%B7.flac?table=block&id=2b0b74f2-17ba-804f-a956-c04708589da5&spaceId=893be7b5-a484-4f7e-95dc-b29a7334966a&expirationTimestamp=1763589600000&signature=vIhHrBZLrk9zOYOfBeNt2yA4RbBnWygvQNTrsdzaqd4&downloadName=%E5%8D%95%E4%BE%9D%E7%BA%AF+-+%E6%83%B3%E4%BD%A0%E6%97%B6%E9%A3%8E%E8%B5%B7.flac"
+  },
+  {
+    title: "第57次取消发送",
+    artist: "陆绮菲",
+    url: "https://file.notion.so/f/f/893be7b5-a484-4f7e-95dc-b29a7334966a/a68cc64e-37f9-4445-9bdf-c31798f13d50/%E9%99%86%E7%BB%AE%E8%8F%B2_-_%E7%AC%AC57%E6%AC%A1%E5%8F%96%E6%B6%88%E5%8F%91%E9%80%81.mp3?table=block&id=2b0b74f2-17ba-802f-a1ec-eff9d560b737&spaceId=893be7b5-a484-4f7e-95dc-b29a7334966a&expirationTimestamp=1763589600000&signature=d6NQ9Tmp7F2mtczHHxZMwlLowriMU54L8tjzgRRowPk&downloadName=%E9%99%86%E7%BB%AE%E8%8F%B2+-+%E7%AC%AC57%E6%AC%A1%E5%8F%96%E6%B6%88%E5%8F%91%E9%80%81.mp3"
+  },
+  {
+    title: "真夜中のドア",
+    artist: "中島愛",
+    url: "https://file.notion.so/f/f/893be7b5-a484-4f7e-95dc-b29a7334966a/b7435503-d152-42c1-8f4f-3620cd0b8f02/%E4%B8%AD%E5%B3%B6%E6%84%9B__%E7%9C%9F%E5%A4%9C%E4%B8%AD%E3%81%AE%E3%83%88%E3%82%99%E3%82%A2.flac?table=block&id=2b0b74f2-17ba-803f-abb9-d449d11854da&spaceId=893be7b5-a484-4f7e-95dc-b29a7334966a&expirationTimestamp=1763589600000&signature=44sQywwUA2SAi5-_Sq_uPemF4o7dYb1VAVLzzOR9fdc&downloadName=%E4%B8%AD%E5%B3%B6%E6%84%9B+%E2%80%93+%E7%9C%9F%E5%A4%9C%E4%B8%AD%E3%81%AE%E3%83%88%E3%82%99%E3%82%A2.flac"
+  },
+  {
+    title: "没出息",
+    artist: "网络歌手",
+    url: "https://file.notion.so/f/f/893be7b5-a484-4f7e-95dc-b29a7334966a/f1ef70c3-a5e2-4daf-a0aa-d952c0c59428/%E6%B2%A1%E5%87%BA%E6%81%AF.m4a?table=block&id=2b0b74f2-17ba-8015-abb7-c62ef9462878&spaceId=893be7b5-a484-4f7e-95dc-b29a7334966a&expirationTimestamp=1763589600000&signature=_ptpejdfmnNN8VTntpQAHyi22_rtc7xdefdikujYJkk&downloadName=%E6%B2%A1%E5%87%BA%E6%81%AF.m4a"
+  },
+  {
+    title: "歌曲名称 3",
+    artist: "艺术家 3",
+    url: "https://example.com/music3.mp3"
+  }
+];
